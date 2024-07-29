4d489f098077868e66e9b6060638f564afd64502
Dalton-K-Hughes <dhughes3223@gmail.com>
2024-07-29 17:00:31 -0400

    Added some base models for my database in models.py

--- website/models.py
+++ website/models.py
@@ -1,21 +1,17 @@
 from . import db
 from flask_login import UserMixin
 
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.Text, nullable=False)
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
+    classSection = db.Column(db.String(150), nullable=False)
+    admin = db.Column(db.Boolean, default=False)
 
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=False)
-
 
 class Assignment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
