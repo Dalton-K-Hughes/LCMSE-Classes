63c8ac333c75ea910d7b01c28c5dd2c343d79613
Dalton-K-Hughes <dhughes3223@gmail.com>
2024-07-31 21:33:21 -0400

    Updated models.py with tables to better represent class structure and users

--- website/models.py
+++ website/models.py
@@ -5,13 +5,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
-    classSection = db.Column(db.String(150), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('class_info.id'), nullable=False)
     admin = db.Column(db.Boolean, default=False)
+    
+class ClassInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    users = db.relationship('User', backref='class', lazy=True)
+    content = db.relationship('Content', backref='class', lazy=True)
+    assignments = db.relationship('Assignment', backref='class', lazy=True)
+    
 
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('class_info.id'))
 
 class Assignment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +28,5 @@ class Assignment(db.Model):
     description=db.Column(db.Text, nullable=False)
     start_date=db.Column(db.DateTime, nullable=False)
     due_date=db.Column(db.DateTime, nullable=False)
+    class_id =db.Column(db.Integer, db.ForeignKey('class_info.id'), nullable=False)
+    
